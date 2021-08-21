90e51922f23fe1fa8348c8bc8408f62e29bc031d
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-21 15:10:43 -0300

    passed the worm to typeorm

--- src/modules/database/implementations/IUser.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-    _id: any;
-    name: string;
-    email: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
-};
--- src/modules/database/implementations/IUserDocument.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface IUserDocument extends Document {
-    name: string;
-    email: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
-};
