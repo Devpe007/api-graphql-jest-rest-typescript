8780df2e0f01db3eebd7a166eca51fd91c3024fa
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-21 14:46:57 -0300

    created IUser interface implementations User migration

--- src/modules/database/connect.ts
+++ src/modules/database/connect.ts
@@ -1,3 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://AndDev07:<password>@api-graphql-jest.snruk.mongodb.net/api_graphql_jest?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://AndDev07:<password>@api-graphql-jest.snruk.mongodb.net/api_graphql_jest?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
---
+++ src/modules/database/implementations/IUser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    _id: any;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
