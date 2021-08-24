cb790837d06f7d8914efa065bac1bed6ab00bcd0
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-23 22:39:33 -0300

    created user-repository

---
+++ src/modules/repositories/user-repository.ts
@@ -0,0 +1,5 @@
+import { User } from "@modules/entities/User";
+import { EntityRepository, Repository } from "typeorm";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {};
