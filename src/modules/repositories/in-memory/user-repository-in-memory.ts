3e151ed4580a5c79feb9293436aa52b91405f62d
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-24 18:03:44 -0300

    created user-repository-in-memory

---
+++ src/modules/repositories/in-memory/user-repository-in-memory.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid } from 'uuid';
+
+import { User } from "@modules/entities/User";
+import { IUserResopositoryInMemory } from "./interface/IUserRespositoryInMemory";
+
+export class UserRepositoryInMemory implements IUserResopositoryInMemory{
+    private users: User[] = [];
+
+    async createUser(user: User): Promise<User> {
+        Object.assign(user, {
+            id: uuid(),
+        });
+
+        this.users.push(user);
+        return user;
+    };
+
+    async findUser(email: string): Promise<boolean> {
+        const user = this.users.some((user) => user.email === email);
+        return user;
+    };
+};
